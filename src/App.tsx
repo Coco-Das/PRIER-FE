a5ac0471fc4097e3f5f8b7098c8d1860b6ee1988
potatoj1n <appabomul@gmail.com>
2024-05-29 17:18:45 +0900

    feat : 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../src/styles/App.css';
+import Routers from './routers/Router';
 function App() {
   return (
     <div className="App">
-      <p>폰트 확인</p>
+      <Routers />
     </div>
   );
 }
