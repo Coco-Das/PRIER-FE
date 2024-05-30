7e6ef2e26a81a2c9140d882781957860057dd2fa
potatoj1n <appabomul@gmail.com>
2024-05-30 16:40:33 +0900

    feat :프로젝트 미리보기 창

---
+++ src/components/user/UserStyle.ts
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components';
+
+export const ProjectContainer = styled.div`
+  border: none;
+  border-radius: 15px;
+  padding: 3px;
+  width: max-content;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px rgba(0, 0, 0, 0.1);
+`;
+export const LinkText = styled.p`
+  font-size: 15px;
+  font-weight: 300;
+  color: #828282;
+`;
