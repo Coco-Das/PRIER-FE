de34a63e26df3939ad75feaa9928132f85600145
potatoj1n <appabomul@gmail.com>
2024-06-04 20:05:40 +0900

    feat : 카카오 로그인 2,3단계 구현( 인증 토큰 처리)

---
+++ src/pages/user/auth/KakaoLoading.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { Loading } from '../../../components/utils/Loading';
+
+export default function KakaoLoading() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get('code');
+    if (code) {
+      // 인가 코드를 백엔드 서버로 전송하여 액세스 토큰을 요청
+      axios
+        .get(`http://localhost:8080/kakao/callback?code=${code}`, { code })
+        .then(response => {
+          console.log(response.data);
+          // 사용자 정보를 로컬 스토리지에 저장하거나 상태 관리
+          const ACCESS_TOKEN = response.data.accessToken;
+          localStorage.setItem('token', ACCESS_TOKEN);
+          navigate('/main');
+        })
+        .catch(error => {
+          console.error('카카오 로그인 실패', error);
+          navigate('/login');
+        });
+    }
+  }, []);
+  return (
+    <>
+      <Loading></Loading>
+    </>
+  );
+}
