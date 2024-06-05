e3f02ccb7e21f9f23be135b448aea737a3595f81
potatoj1n <appabomul@gmail.com>
2024-06-05 18:45:32 +0900

    feat : 로그아웃 api 작성

--- src/services/UserApi.ts
+++ src/services/UserApi.ts
@@ -1,17 +1,15 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useUserStore } from '../states/user/UserStore';
-
-const API_BASE_URL = '/api';
+import { API_BASE_URL, KAKAO_ACCESS_TOKEN } from '../const/TokenApi';
+import { useNavigate } from 'react-router-dom';
 
 export function FetchMyPage() {
   const setUserProfile = useUserStore(state => state.setUserProfile);
   useEffect(() => {
     const FetchUserProfile = async () => {
       try {
-        const response = await axios.get(`${API_BASE_URL}/users/mypage`, {
-          headers: { Authorization: 'Bearer 1eed2de1-f7fd-6cb0-3bab-932e763390ec' },
-        });
+        const response = await API_BASE_URL.get('/users/mypage');
         console.log('유저 정보 요청 성공', response.data);
         return response.data;
       } catch (error) {
@@ -21,4 +19,28 @@ export function FetchMyPage() {
     };
     FetchUserProfile();
   }, [setUserProfile]);
+}
+
+export function FetchLogout() {
+  const navigate = useNavigate();
+  const setLogout = useUserStore(state => state.setLogout);
+
+  return useCallback(async () => {
+    try {
+      const response = await axios.get('https://kauth.kakao.com/oauth/logout', {
+        headers: { Authorization: `Bearer ${KAKAO_ACCESS_TOKEN}` },
+      });
+      console.log('로그아웃 요청 성공', response.data);
+
+      localStorage.removeItem('accessToken');
+      localStorage.removeItem('kakao_access_token');
+      localStorage.removeItem('userId');
+
+      setLogout();
+      navigate('/');
+    } catch (error) {
+      console.error('로그아웃 요청 실패:', error);
+      throw error;
+    }
+  }, [navigate, setLogout]);
 }
