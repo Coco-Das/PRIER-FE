26c6564297b28267bf6a732207ded6a4c0fd70c1
potatoj1n <appabomul@gmail.com>
2024-06-13 00:33:35 +0900

    feat : 메인페이지 모든 프로젝트 기본 렌더링

--- src/components/user/UserStyle.ts
+++ src/components/user/UserStyle.ts
@@ -4,7 +4,7 @@ import { ReactComponent as PointIcon } from '../../assets/Coin.svg';
 export const ProjectContainer = styled.div`
   border: none;
   border-radius: 15px;
-  padding: 3px;
+  padding: 0.5rem;
   width: max-content;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px rgba(0, 0, 0, 0.1);
 `;
