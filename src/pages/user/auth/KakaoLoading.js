fb588242bca5fd688385a71a180836b7249392e0
rbfl1260 <seven0126@naver.com>
2024-06-19 14:49:29 +0900

    주소 설정

--- src/pages/user/auth/KakaoLoading.js
+++ src/pages/user/auth/KakaoLoading.js
@@ -15,7 +15,7 @@ export default function KakaoLoading() {
       const fetchData = async () => {
         console.log('fetchData:');
         try {
-          const response = await axios.get(`http://13.209.97.23:8080/api/kakao/callback?code=${code}`);
+          const response = await axios.get(`http://43.203.248.74:8080/api/kakao/callback?code=${code}`);
 
           console.log('데이터', response.data);
           const ACCESS_TOKEN = response.data.accessToken;
