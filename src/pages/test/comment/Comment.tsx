29692df1f4a3047d1281322b3ebb7f71cb249aea
rbfl1260 <seven0126@naver.com>
2024-06-19 14:48:23 +0900

    feat: 댓글창 사이드바

--- src/pages/test/comment/Comment.tsx
+++ src/pages/test/comment/Comment.tsx
@@ -1,18 +1,34 @@
 import { useParams } from 'react-router-dom';
 import { useProjectStore } from '../../../states/projects/ProjectStore';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { API_BASE_URL } from '../../../const/TokenApi';
+import PropTypes from 'prop-types';
+import { Button, CommentDiv, CommentWrapper, SidebarContainer } from './CommentStyles';
 
 //댓글 불러옴 get
 //댓글 등록 post
+interface CommentProps {
+  show: boolean;
+  onMouseLeave?: () => void;
+}
 
-export const Comment = () => {
+interface CommentData {
+  commentId: number;
+  content: string;
+  isMine: boolean;
+  score: number;
+  userId: number;
+  userName: string;
+}
+
+export const Comment: React.FC<CommentProps> = ({ show, onMouseLeave }) => {
   const { projectId } = useParams<{ projectId: string }>();
   const setProjectId = useProjectStore(state => state.setProjectId);
+  const [comments, setComments] = useState<CommentData[]>([]);
 
   useEffect(() => {
     if (projectId) {
-      setProjectId(projectId); // URL 파라미터로부터 projectId를 상태로 설정
+      setProjectId(projectId);
     }
   }, [projectId, setProjectId]);
 
@@ -22,7 +38,8 @@ export const Comment = () => {
     try {
       const response = await API_BASE_URL.get(`/projects/${projectId}/comment`);
       const Data = response.data;
-      console.log(Data);
+      setComments(Data);
+      console.log(comments);
     } catch (error) {
       console.error('에러:', error);
     }
@@ -30,7 +47,30 @@ export const Comment = () => {
 
   useEffect(() => {
     handleGetComments();
-  }, [projectId]);
+  }, [show]);
 
-  return <div>리뷰페이지</div>;
+  return (
+    <SidebarContainer show={show}>
+      <div style={{ width: '100%', height: '5%', display: 'flex' }}>
+        <Button>전체 댓글</Button>
+        <Button style={{}}>나의 댓글</Button>
+      </div>
+      <CommentDiv style={{ height: '95%' }}>
+        {comments.map(comment => (
+          <CommentWrapper key={comment.commentId}>
+            <p>
+              <strong>{comment.userName}</strong>
+            </p>
+            <p>{comment.content}</p>
+            <p>Score: {comment.score}</p>
+          </CommentWrapper>
+        ))}
+      </CommentDiv>
+    </SidebarContainer>
+  );
+};
+
+Comment.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onMouseLeave: PropTypes.func.isRequired,
 };
