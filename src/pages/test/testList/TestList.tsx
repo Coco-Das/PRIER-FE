4c4142ef9c890b23e2a7b78fef7659c4dcaa8b33
rbfl1260 <seven0126@naver.com>
2024-06-20 17:05:04 +0900

    feat: testlist생성

--- src/pages/test/testList/TestList.tsx
+++ src/pages/test/testList/TestList.tsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FilterBtn, ListWrapper } from './TestListStyles';
 
 function TestList() {
-  return <div></div>;
+  const [filter, setFilter] = useState<'latest' | 'oldest' | 'active'>('latest');
+  const handleFilterChange = (filter: 'latest' | 'oldest' | 'active') => {
+    setFilter(filter);
+  };
+  return (
+    <ListWrapper>
+      <span className="ml-4 font-extrabold mt-3" style={{ wordBreak: 'break-word', color: '#315AF1' }}>
+        누구의? 프로젝트
+      </span>
+      <div className="ml-5 mt-2" style={{ display: 'flex', gap: '15px', width: '90%', height: '100%' }}>
+        <FilterBtn>최신순</FilterBtn>
+        <FilterBtn>등록순</FilterBtn>
+        <FilterBtn>진행중인 프로젝트만 보기</FilterBtn>
+      </div>
+    </ListWrapper>
+  );
 }
 
 export default TestList;
