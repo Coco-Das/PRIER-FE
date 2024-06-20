461538945cb604b99c5c6f507da8c7cc798b11c7
potatoj1n <appabomul@gmail.com>
2024-06-20 16:13:43 +0900

    feat : 다른 사람 프로필 요청 API, 스토어 생성

--- src/states/user/UserStore.tsx
+++ src/states/user/UserStore.tsx
@@ -74,29 +74,7 @@ const initialProfile: UserProfile = {
   balance: 0,
 };
 export const useUserStore = create<UserStore>(set => ({
-  userProfile: {
-    nickname: '',
-    belonging: '',
-    rank: '',
-    email: '',
-    blog: '',
-    github: '',
-    figma: '',
-    notion: '',
-    intro: '',
-    firstQuest: false,
-    secondQuest: false,
-    thirdQuest: false,
-    nowProjectId: 0,
-    nowProjectName: '',
-    nowProjectTeamName: '',
-    nowProjectFeedbackCount: 0,
-    nowProjectScore: 0,
-    nowProjectStaticPercentage: '',
-    nowProjectKeywordList: [],
-    myPageCommentDtoList: [],
-    balance: 0,
-  },
+  userProfile: initialProfile,
   setUserProfile: (profile: UserProfile) => {
     sessionStorage.setItem('nickname', profile.nickname);
     set({ userProfile: profile });
@@ -145,5 +123,80 @@ export const useUserStore = create<UserStore>(set => ({
     set(state => ({
       userProfile: { ...state.userProfile, quest },
     }));
+  },
+}));
+
+//남의 프로필 스토어
+interface OtherProjectKeyword {
+  content: string;
+  count: number;
+}
+
+interface OtherComment {
+  projectId: number;
+  commentId: number;
+  projectName: string;
+  teamName: string;
+  content: string;
+  score: number;
+}
+
+interface OtherProfile {
+  nickname: string;
+  belonging: string | null;
+  rank: string;
+  email: string;
+  blog: string | null;
+  github: string | null;
+  figma: string | null;
+  notion: string | null;
+  intro: string | null;
+  firstQuest: boolean;
+  secondQuest: boolean;
+  thirdQuest: boolean;
+  nowProjectId: number;
+  nowProjectName: string;
+  nowProjectTeamName: string;
+  nowProjectFeedbackCount: number;
+  nowProjectScore: number;
+  nowProjectStaticPercentage: string;
+  nowProjectKeywordList: OtherProjectKeyword[];
+  myPageCommentDtoList: OtherComment[];
+  balance: number;
+}
+
+interface OtherProfileStore {
+  otherProfile: OtherProfile;
+  setOtherProfile: (profile: OtherProfile) => void;
+}
+
+const initialOtherProfile: OtherProfile = {
+  nickname: '',
+  belonging: '',
+  rank: '',
+  email: '',
+  blog: '',
+  github: '',
+  figma: '',
+  notion: '',
+  intro: '',
+  firstQuest: false,
+  secondQuest: false,
+  thirdQuest: false,
+  nowProjectId: 0,
+  nowProjectName: '',
+  nowProjectTeamName: '',
+  nowProjectFeedbackCount: 0,
+  nowProjectScore: 0,
+  nowProjectStaticPercentage: '',
+  nowProjectKeywordList: [],
+  myPageCommentDtoList: [],
+  balance: 0,
+};
+
+export const useOtherProfileStore = create<OtherProfileStore>(set => ({
+  otherProfile: initialOtherProfile,
+  setOtherProfile: (profile: OtherProfile) => {
+    set({ otherProfile: profile });
   },
 }));
