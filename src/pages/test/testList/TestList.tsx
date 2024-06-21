3ce9962e0edb6caf22068ceba4828bfc993f85ab
rbfl1260 <seven0126@naver.com>
2024-06-21 14:26:31 +0900

    feat: 페이지 설정 및 프로젝트 디자인

--- src/pages/test/testList/TestList.tsx
+++ src/pages/test/testList/TestList.tsx
@@ -1,20 +1,218 @@
-import React, { useState } from 'react';
-import { FilterBtn, ListWrapper } from './TestListStyles';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  DivWrapper,
+  FilterBtn,
+  GreenDiv,
+  ImageWrapper,
+  Img,
+  ListDiv,
+  ListWrapper,
+  PurpleDiv,
+  Tag,
+  TagWrapper,
+} from './TestListStyles';
+import { useOtherProfileStore } from '../../../states/user/UserStore';
+import { useParams } from 'react-router-dom';
+import { API_BASE_URL } from '../../../const/TokenApi';
+
+interface Tag {
+  tagId: number;
+  tagName: string;
+  color: string;
+}
+
+interface Project {
+  projectId: number;
+  title: string;
+  teamName: string;
+  mainImageUrl: string;
+  tags: Tag[];
+  score: number;
+  devStartDate: string;
+  status: string;
+  link: string;
+}
 
 function TestList() {
-  const [filter, setFilter] = useState<'latest' | 'oldest' | 'active'>('latest');
-  const handleFilterChange = (filter: 'latest' | 'oldest' | 'active') => {
-    setFilter(filter);
+  const { userId } = useParams<{ userId: string }>();
+  const userProfile = useOtherProfileStore(state => state.otherProfile);
+  const [filter, setFilter] = useState<'0' | '1' | '2'>('2');
+  const [page, setPage] = useState(0);
+  const [projects, setProjects] = useState<Project[]>([]);
+  const [totalPages, setTotalPages] = useState(0);
+  // const [totalElements, setTotalElements] = useState(0);
+  const colors = ['#FFD09B', '#CEE7FF', '#E1F9F0'];
+
+  const handleFilterChange = (newFilter: '0' | '1' | '2') => {
+    setFilter(newFilter);
   };
+
+  useEffect(() => {
+    if (userId) {
+      handleGetInfo(userId, filter, page);
+    }
+  }, [userId, filter, page]);
+
+  const handleGetInfo = async (userId: string, filter: '0' | '1' | '2', page: number) => {
+    try {
+      const response = await API_BASE_URL.get(`/projects/user-projects?userId=${userId}&filter=${filter}&page=${page}`);
+      const data = response.data;
+
+      setProjects(data.content);
+      setTotalPages(data.totalPages);
+      // setTotalElements(data.totalElements);
+
+      const storedColors = getTagColors();
+      const projectsWithColoredTags = data.content.map((project: Project) => {
+        const coloredTags = project.tags.map(tag => {
+          const color = storedColors[tag.tagName] || getRandomColor();
+          if (!storedColors[tag.tagName]) {
+            storedColors[tag.tagName] = color;
+          }
+          return { ...tag, color };
+        });
+        return { ...project, tags: coloredTags };
+      });
+
+      setProjects(projectsWithColoredTags);
+      saveTagColors(storedColors);
+      console.log(userId);
+      console.log(page);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const saveTagColors = (tagColors: { [key: string]: string }) => {
+    localStorage.setItem('tagColors', JSON.stringify(tagColors));
+  };
+
+  const getTagColors = (): { [key: string]: string } => {
+    const tagColors = localStorage.getItem('tagColors');
+    return tagColors ? JSON.parse(tagColors) : {};
+  };
+
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
+  const getStatusText = (status: string) => {
+    switch (status) {
+      case 'DEPLOYMENT_COMPLETE':
+        return '배포 완료';
+      case 'DEVELOPING':
+        return '개발 중';
+      case 'PLANNING':
+        return '기획 중';
+      default:
+        return status;
+    }
+  };
+
+  const renderPagination = () => {
+    const pages = [];
+    for (let i = 0; i < totalPages; i++) {
+      pages.push(
+        <button
+          key={i}
+          onClick={() => {
+            setPage(i);
+          }}
+          style={{
+            borderRadius: '8px',
+            margin: '0 5px',
+            padding: '5px 10px',
+            background: i === page ? '#315AF1' : '#fff',
+            color: i === page ? '#fff' : '#000',
+          }}
+        >
+          {i + 1}
+        </button>,
+      );
+    }
+    return pages;
+  };
+
   return (
     <ListWrapper>
       <span className="ml-4 font-extrabold mt-3" style={{ wordBreak: 'break-word', color: '#315AF1' }}>
-        누구의? 프로젝트
+        {userProfile.nickname} 프로젝트
       </span>
       <div className="ml-5 mt-2" style={{ display: 'flex', gap: '15px', width: '90%', height: '100%' }}>
-        <FilterBtn>최신순</FilterBtn>
-        <FilterBtn>등록순</FilterBtn>
-        <FilterBtn>진행중인 프로젝트만 보기</FilterBtn>
+        <FilterBtn $isActive={filter === '2'} onClick={() => handleFilterChange('2')}>
+          최신순
+        </FilterBtn>
+        <FilterBtn $isActive={filter === '1'} onClick={() => handleFilterChange('1')}>
+          등록순
+        </FilterBtn>
+        <FilterBtn $isActive={filter === '0'} onClick={() => handleFilterChange('0')}>
+          진행중인 프로젝트만 보기
+        </FilterBtn>
+      </div>
+      <div className="mt-5" style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+        {projects.map(project => (
+          <ListDiv key={project.projectId}>
+            <ImageWrapper>
+              <p style={{ fontSize: '20px', fontWeight: 'bold' }}>{project.title}</p>
+              <div
+                style={{
+                  width: '100%',
+                  display: 'flex',
+                  color: '#828282',
+                  fontSize: '14px',
+                }}
+              >
+                <span>{project.devStartDate} ~ </span>
+                <span style={{ color: getStatusText(project.status) === '배포 완료' ? 'inherit' : '#315AF1' }}>
+                  {getStatusText(project.status)}
+                </span>
+                <span className="underline" style={{ marginLeft: 'auto', paddingRight: '10px' }}>
+                  자세히 보기&gt;
+                </span>
+              </div>
+              {/* 이미지 있을 때만 보이도록 */}
+              <div style={{ padding: '10px 10px 0px 0px', height: '100%', width: '100s%' }}>
+                {project.mainImageUrl && <Img src={project.mainImageUrl} alt={project.title} />}
+              </div>
+            </ImageWrapper>
+            <DivWrapper>
+              <PurpleDiv>
+                <p className="font-bold">소속</p>
+                <p className="font-bold" style={{ fontSize: '16px' }}>
+                  {project.teamName} 팀
+                </p>
+                <TagWrapper>
+                  {project.tags.map(tag => (
+                    <Tag key={tag.tagId} $bgColor={tag.color}>
+                      {tag.tagName}
+                    </Tag>
+                  ))}
+                </TagWrapper>
+              </PurpleDiv>
+              <GreenDiv onClick={() => window.open(project.link, '_blank', 'noopener,noreferrer')}>
+                <p>배포 링크</p>
+                <span
+                  className="font-bold underline"
+                  style={{ marginLeft: 'auto', fontSize: '16px', color: '#828282', marginTop: 'auto' }}
+                >
+                  바로가기 &rarr;
+                </span>
+              </GreenDiv>
+            </DivWrapper>
+          </ListDiv>
+        ))}
+      </div>
+      <div
+        style={{
+          marginTop: '20px',
+          display: 'flex',
+          justifyContent: 'center',
+          fontSize: '16px',
+        }}
+      >
+        {renderPagination()}
       </div>
     </ListWrapper>
   );
--- src/pages/test/testList/TestListStyles.tsx
+++ src/pages/test/testList/TestListStyles.tsx
@@ -6,13 +6,93 @@ export const ListWrapper = styled.div`
   display: flex;
   overflow-y: auto;
   flex-direction: column;
-  padding: 0px 30px 0px 30px;
+  padding: 0px 30px 20px 30px;
 `;
-export const FilterBtn = styled.button`
+export const FilterBtn = styled.button<{ $isActive: boolean }>`
   /* width: 10%; */
+  background-color: ${props => (props.$isActive ? '#315af1' : 'white')};
+  color: ${props => (props.$isActive ? 'white' : 'black')};
   height: 100%;
   padding: 5px 10px;
   border-radius: 20px;
   font-size: 12px;
-  background-color: #f6f6f6;
+  &:hover {
+    background-color: ${props => (props.$isActive ? '#315af1' : '#e0e0e0')};
+  }
+`;
+export const ListDiv = styled.div`
+  width: 100%;
+  height: 350px;
+  display: flex;
+  padding: 20px 40px;
+  border: 1px solid #a9b5bc;
+  border-radius: 15px;
+  font-size: 18px;
+  gap: 10px;
+`;
+export const ImageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  height: 100%;
+  /* border: 1px solid black; */
+`;
+export const Img = styled.img`
+  object-fit: cover;
+  border-radius: 15px;
+  width: 100%;
+  height: 80%;
+  /* border: 1px solid red; */
+`;
+export const DivWrapper = styled.div`
+  width: 35%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+export const PurpleDiv = styled.div`
+  background-color: #b193d040;
+  border: 1px solid #e0e0e0;
+  gap: 15px;
+  display: flex;
+  flex-direction: column;
+  height: 50%;
+  width: 100%;
+  border-radius: 8px;
+  padding: 20px 30px;
+  padding-bottom: 0px;
+  position: relative;
+`;
+export const GreenDiv = styled.div`
+  background-color: #e1f9f0;
+  border: 1px solid #e0e0e0;
+  gap: 15px;
+  display: flex;
+  flex-direction: column;
+  height: 50%;
+  width: 100%;
+  border-radius: 8px;
+  padding: 20px 30px;
+  /* padding-bottom: 0px; */
+  font-weight: bold;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+export const TagWrapper = styled.div`
+  padding-left: 30px;
+  display: flex;
+  position: absolute;
+  flex-wrap: nowrap;
+  top: 70%;
+  left: 0%;
+  gap: 5px;
+`;
+export const Tag = styled.div<{ $bgColor: string }>`
+  padding: 5px 10px;
+  position: relative;
+  background-color: ${props => props.$bgColor};
+  border-radius: 20px;
+  font-size: 12px;
 `;
