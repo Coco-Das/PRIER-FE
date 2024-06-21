ebb2fb4b463c23fa76b4aaee28c94e9c6ee31649
potatoj1n <appabomul@gmail.com>
2024-06-21 19:17:12 +0900

    chore : 주소 변경

--- src/const/TokenApi.ts
+++ src/const/TokenApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const KAKAO_ACCESS_TOKEN = localStorage.getItem('kakaoAccessToken');
 
 export const API_BASE_URL = axios.create({
-  baseURL: 'http://52.78.16.255:8080/api',
+  baseURL: 'http://3.35.136.167:8080/api',
 });
 //요청 인터셉터
 API_BASE_URL.interceptors.request.use(
