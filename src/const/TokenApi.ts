bb5e5498171f5f29e42fcdc2090d002e16791d18
rbfl1260 <seven0126@naver.com>
2024-06-26 11:49:26 +0900

    주소 설정

--- src/const/TokenApi.ts
+++ src/const/TokenApi.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const KAKAO_ACCESS_TOKEN = localStorage.getItem('kakaoAccessToken');
 
 export const API_BASE_URL = axios.create({
-  baseURL: 'http://52.79.204.128:8080/api',
+  baseURL: 'http://54.180.163.49:8080/api',
 });
 //요청 인터셉터
 API_BASE_URL.interceptors.request.use(
