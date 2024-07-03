d5b3dd2e3f9d2ca096e5afdcf03eb6476ac08b9b
inji0212 <in17100170@kakao.com>
2024-07-03 12:16:08 +0900

    chore: 사이드바 이름 수정

--- src/components/utils/SideBar.tsx
+++ src/components/utils/SideBar.tsx
@@ -152,9 +152,9 @@ const SideBar: React.FC<SideBarProps> = ({ open, toggleDrawer, currentPath }) =>
   const logout = FetchLogout();
 
   const menuItems = [
-    { text: '프로젝트 리스트', path: '/main', icon: sidebar1 },
-    { text: '테스트 생성하기', path: '/createtest', icon: sidebar2 },
-    { text: '테스트한 프로젝트', path: `/testlist/${localStorage.getItem('userId')}`, icon: sidebar3 },
+    { text: '전체 프로젝트', path: '/main', icon: sidebar1 },
+    { text: '테스트 생성', path: '/createtest', icon: sidebar2 },
+    { text: '내 프로젝트', path: `/testlist/${localStorage.getItem('userId')}`, icon: sidebar3 },
     { text: '커뮤니티', path: '/board', icon: sidebar4 },
     { text: '상점', path: '/store', icon: sidebar5 },
     { text: '마이페이지', path: '/mypage', icon: sidebar6 },
